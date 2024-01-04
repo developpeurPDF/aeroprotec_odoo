102aa8937ada1a238be49b0fe435295fe982be7b
“developpeurPDF” <contact@paudigitalfactory.fr>
2024-01-04 15:49:08 +0100

    m

---
+++ Aero_addons/ad_aero_custom_addons/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+
+from . import product_template
+from . import famille_matiere
+from . import matiere_parameter_value
+from . import model_mrp_aero
