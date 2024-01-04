5045a3cdd22dbc928f3b39c38d34bec4e7c84c9c
“developpeurPDF” <contact@paudigitalfactory.fr>
2024-01-04 18:05:57 +0100

    m3

--- Aero_addons/ad_aero_custom_addons/models/product_template.py
+++ Aero_addons/ad_aero_custom_addons/models/product_template.py
@@ -50,6 +50,7 @@ class ProductTemplate(models.Model):
     programme_aeonautique = fields.Many2one('programme.aeonautique', string="Programme aéronautique",)
     test = fields.Char(string="test")
     test2 = fields.Char(string="test 2")
+    test3 = fields.Char(string="test 3")
 
     def _compute_air(self):
         for rec in self:
